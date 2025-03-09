bc5b05558f4faf4a05ea2468a015ef4edf30c824
Lusifer2602 <2602pranav@gmail.com>
2025-03-09 19:07:44 +0530

    learnt more about variables

--- SM/js/May9th.js
+++ SM/js/May9th.js
@@ -2,7 +2,13 @@
 console.log(typeof undefined);
 console.log(typeof 0);
 console.log (typeof true);
-consol.log( typeof"Iambatman");
 console.log(typeof null);
+console.log(typeof "Iambatman");
 console.log(typeof alert);
-console.log(typeof Symbol('noideawtfthisis');
+console.log(typeof Symbol('ihavenoideawhatthisis'));
+
+//now let check the outputs of these statements
+let name="Bosswhopper";
+console.log(`The name is ${1}`);
+// console.log(`The name is ${"name"}`);  //this will throw error because you can't declare variable for a string under ` ` bracket
+console.log(`The name is ${name}`);
