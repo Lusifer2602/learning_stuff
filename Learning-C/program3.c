8469afa70f5d34c07220b4fc80746011a6a9d229
Lusifer2602 <2602pranav@gmail.com>
2025-03-18 09:54:25 +0530

    created third program for assignment

---
+++ Learning-C/program3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a,b,c,ch;
+    printf("\nEnter two values :");
+    scanf("%d%d", &a, &b);
+    printf("\nEnter the choice:\n1.Addition\n2.Suntraction\n3.Multiplaction\n4.Division\n5.Modulo\nchoice: ");
+    scanf("%d", &ch);
+    switch(ch)
+{
+    case 1: printf("\nAddition of %d+%d is %d", a, b, a+b);
+    break;
+    case 2:printf("\nSubtraction of %d-%d is %d", a, b, a-b);
+    break;
+    case 3:printf("\nMultiplication of %d*%d is %d", a, b, a*b);
+    break;
+    case 4:if(b==0)
+    printf("\nDivisionis not possible");
+    else
+    printf("\nDivision od %d/%d is %f", a, b,(float)a/b);
+    break;
+    case5:printf("\nModulus of %d%%d is %d", a, b, a%b);
+    break;
+    deafult: printf("\nEnter correct choice:");
+    break;
+}
+getch();
+}
