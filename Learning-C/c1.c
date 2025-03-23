a1ad902223dc5ac6e2df6b0ae1a3b5cb4f9c6fb9
Lusifer2602 <2602pranav@gmail.com>
2025-03-24 04:59:35 +0530

    moved assignment's files to a new folder practiced C (operators, syntax fundamentals)

---
+++ Learning-C/c1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+        char str[]="Hello World\n"; //if you think of using single ' ', don't, it is an invalid initialiser
+        printf("%s", str);
+        return(0);
+}
---
+++ Learning-C/operators.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+        int a=10, b=123;
+        int sum=a+b;
+            printf("SOS ke kitne baap?!\n SOS ke %d baap.\n", sum);
+            return 0;
+}
