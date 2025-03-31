88d4c76f62f2c9d22b2a6ab8ebda4e45531a4c36
Lusifer2602 <2602pranav@gmail.com>
2025-04-01 00:02:10 +0530

    else if conditionals done

--- SM/js/mar31.js
+++ SM/js/mar31.js
@@ -1,6 +1,6 @@
 //conditional branching : if
 //if evaluates a given condition and the given code block is executed if the condition is true
-/*
+
 let a=prompt("Enter the year we are in : ");
 if (a==2025) {
     console.log("CORRECT!!!");
@@ -14,5 +14,16 @@ if (age >=18) {
     console.log("Allowed");
 } else {
     console.log("balak h laadle dur reh");
-} */
+}
 
+//Several conditions : "else if"
+let year1=prompt("SeedheMaut or Chaar Diwaari ke concert kis date par gaya tha?");
+if (year1=="29/03/2025"){
+    console.log("AYYEAA SAALE YAAD HAI!!!!");
+}
+else if (year1==NaN){
+    console.log("Abbe bhai koi response to deta!!!");
+}
+else {
+    console.log("Bhool gaya bc!?!?")
+} //else if in JS is just like elif in python no biggie
