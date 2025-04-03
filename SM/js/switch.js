2725a9380fc865ad7017f15f0c4137f5ced62079
Lusifer2602 <2602pranav@gmail.com>
2025-04-04 00:22:27 +0530

    switches learning started

--- SM/js/switch.js
+++ SM/js/switch.js
@@ -1,1 +1,30 @@
+console.log("Chalo baccho aaj seekhenge ki kaise hum switch case statements ko JavaScript mein use kar sakte hain.")
+
+/* syntax :
+switch(x) {
+  case 'value1':  // if (x === 'value1')
+    ...
+    [break]
+
+  case 'value2':  // if (x === 'value2')
+    ...
+    [break]
+
+  default:
+    ...
+    [break]     } 
+*/
+//example
+let a=prompt("Bhayanak Aatma");
+swtich(a){
+    case "Bhayanak Aatma":
+    console.log("Tumhari Beti ke andar ek bhayanak aatma hai!!! ")
+    break;
     
+    case "Batman";
+    console.log("Kabhi kuchh or bhi likha lia kar bhadwe");
+    break;
+    case "Spiderman";
+    console.log("Hehehe fav superhero.");
+    break;
+}
