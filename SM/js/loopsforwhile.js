898bdd631c427ba8fd6b69e1acb299ca93a6cd98
Lusifer2602 <2602pranav@gmail.com>
2025-04-04 20:04:39 +0530

    learnt thoda sa while loops working on DOM

---
+++ SM/js/loopsforwhile.js
@@ -0,0 +1,20 @@
+/*syntax
+while loop:
+
+while(condition){
+    //code
+    //loop body
+}
+*/
+// let i=1;
+// while(i<11){
+//     console.log(i);
+//     i++;
+// }
+// console.log("Here the while loop ends.")
+
+let i = 3;
+while (i) { // when i becomes 0, the condition becomes falsy, and the loop stops
+  console.log( i );
+  i--;
+}
