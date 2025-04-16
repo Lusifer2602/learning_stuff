04864321b5bd3ae2cef2b61ea7834057d6fd253c
Lusifer2602 <2602pranav@gmail.com>
2025-04-17 01:15:00 +0530

    learnt that variables can be declared in c and assigned a value later

---
+++ Learning-C/variables.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int asshole; //declared the var asshole
+    printf("One of the greatest thing about C is you are able to declare variables without assigning a value to them.\n");
+    asshole=123; //assigned valye to the variable later! ain't that crazy!?!?
+    printf("The integer assigned is : %d", asshole);
+    return 0;
+}
