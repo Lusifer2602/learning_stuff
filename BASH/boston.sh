c0889fd41bb2c9cee40907b94436995205f0c988
Lusifer2602 <2602pranav@gmail.com>
2025-04-21 03:09:07 +0530

    learnt about using variables universally in bash scripts

---
+++ BASH/boston.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+#learning new var called random
+
+echo "So here we print the built in commands !!!"
+sleep 2
+echo "The random number generated is $RANDOM
+
+And we are working in the shell $SHELL.
+
+You are logged in here as $USER
+
+and working inside the directory $PWD
+
+Also the hostname is $HOSTNAME."
