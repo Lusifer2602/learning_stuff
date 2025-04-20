869bd17048abc6b0ef3137530561b75e722d37a0
Lusifer2602 <2602pranav@gmail.com>
2025-04-21 01:01:21 +0530

    started learning bash

---
+++ BASH/bestdayever.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+name="Bosswhopper"
+echo "Good morning $name !!!"
+
+sleep 2
+
+echo "Ram ram bhai dhyaan rakh"
+sleep 1
+
+#now we learn to take input here in the statement
+echo "Plese state your name sir"
+
+echo "Hello Master $brucewayne
+Everyone's been waiting for you."
+
+#lets see how to make the script answer before it is being in the moment
+prashn=$1
+echo ("RAM RAM $prashn BHAI ! ! ! !") 
---
+++ BASH/himom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Hi mom"
+sleep 3
+echo "uhh huh"
+sleep 2
+echo "abbe jaa na bakwas kar rha!!!!"
