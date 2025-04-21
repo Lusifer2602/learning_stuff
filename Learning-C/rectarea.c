47e2dc396e8dd2704e444b6d0821052aa567faa5
Lusifer2602 <2602pranav@gmail.com>
2025-04-21 13:51:50 +0530

    created programs to lowkey ovreall involve everything learnt so far

---
+++ Learning-C/rectarea.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+   int  length = 32;
+   int breadth = 65;
+   int area=length*breadth;
+   printf("Length of the Rectange is : %d\n", length);
+
+   printf("Breadth of the Rectange is : %d\n", breadth);
+
+   printf("Area of the Rectange is : %dsq.units\n", area);
+}
--- Learning-C/variables.c
+++ Learning-C/variables.c
@@ -3,6 +3,27 @@ int main(){
     int asshole; //declared the var asshole
     printf("One of the greatest thing about C is you are able to declare variables without assigning a value to them.\n");
     asshole=123; //assigned valye to the variable later! ain't that crazy!?!?
-    printf("The integer assigned is : %d", asshole);
+    printf("The integer assigned is : %d\n", asshole);
+
+//ok lets create some shit that involves all learnt about variables
+/*
+Baba Mastnath ki details pe ek program banate hain
+bmu name
+city
+college rating i give
+rating to students i 'd give
+average age of students
+ */ 
+char bmuname[]="Baba Mastnath Univeristy";
+float bmurating=4.3;
+int avgage=20;
+char bmuclass='d';
+int my_year=1;
+
+printf("The Name is %s\n", bmuname);
+printf("I'd rate BMU %f/10\n", bmurating);
+printf("Average age that I observed in this is : %d\n", avgage);
+
+
     return 0;
 }
