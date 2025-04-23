c381ca9ae2a90228bc4d52faeda7733c3f821cc9
Lusifer2602 <2602pranav@gmail.com>
2025-04-24 00:07:36 +0530

    yaha kari hai shuru type conversion pichhle wale mein nahi

---
+++ Learning-C/typeconversion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+//We are going to perform type conversion now
+int main (){
+    int x=23;
+    int y=5;
+    int z=76;
+
+printf("x int is : %d\n", x);
+//conversion happens implicit and explicit
+
+//implicit conversion happen in situations like these
+// printf("Printing int y as float %f\n", y); //this is an error statement that gives error in the name of type conversion and i hope you let w3school know about it or maybe I am just dumn lol
+
+float sum=312+3213;
+printf("This is the float sum of int 312 and 3213 : %f\n", sum);
+
+return 0;
+}
