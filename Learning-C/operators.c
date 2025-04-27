d1aff8d9f15da7519c5515b7d047eb4317996898
Lusifer2602 <2602pranav@gmail.com>
2025-04-27 18:03:47 +0530

    learning operators

--- Learning-C/operators.c
+++ Learning-C/operators.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 int main() {
+
         int a=10, b=123;
+
         int sum=a+b;
             printf("SOS ke kitne baap?!\nSOS ke %d baap.\n", sum);
             printf("We can also add variables in print statement,\nlike here a+b is %d\n", a+b);
-            return 0;
+        int diff=a-b;
+            printf("this is the number of fucks i give about Pakistan's sentiments if they're going to keep hurting ourpeople just like this %d\n", diff);
+        int prod=a*b;
+            printf("Bhai %d baar to aajkal potty aa rhi\n", prod);
+        float quot=a/b;
+            printf("I could kill for %f an ounce of happiness\n", quot);
+        int remainder=a%b;
+            printf("Bhai 123 ko 10 se divide karoge to %d remainder ayega.\n", remainder);
+        int incrementoperator=++a;//this operator will increase value of any function by 1.
+            printf("a par a++ lagayenge to banega %d\n", incrementoperator);
+            //thoda mamla samajh nahi aa rha so lets try other stuff on these operators
+            printf("Ye hogaya b pe increment operator apply %d\n", ++b);
+    return 0;
 }
