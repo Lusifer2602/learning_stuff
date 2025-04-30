bbae709f1109b116e76aa01b51bd1e758fb214ad
Lusifer2602 <2602pranav@gmail.com>
2025-04-30 23:42:48 +0530

    completed input output shit learning if else abhi (mummy ne sone ki keh di)

---
+++ Learning-C/learningboosted/if_else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    //lets start with a couple of basic if_else examples
+    int num=110;
+    if (num>10){
+        printf("The variable is bigger than 0\n");
+}
+    int batman=10000;
+    if (batman<1000){
+        printf("Mai hu giyaan mai hu bada taakatwar\n");}
+    else { printf("I AM BATMAN 10000\n");
+    }
+return 0; }
---
+++ Learning-C/learningboosted/ifelsepractice.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a number and we'll see if that shit is +ve, -ve or 0");
+    scanf("%d");
+    if (a=0){
+    printf("The number is a zero value");}
+    
