389ef0974c8aa7fda65e47495aeacf417b527a5e
Lusifer2602 <2602pranav@gmail.com>
2025-05-01 19:36:42 +0530

    yayy pehla if else khud se likh lia

--- Learning-C/learningboosted/ifelsepractice.c
+++ Learning-C/learningboosted/ifelsepractice.c
@@ -2,7 +2,20 @@
 int main(){
     int a;
     printf("Enter a number and we'll see if that shit is +ve, -ve or 0");
-    scanf("%d");
-    if (a=0){
-    printf("The number is a zero value");}
+    scanf("%d", &a);
+    
+    if (a<0){
+    printf("The number is a negative number.\n");
+    }
+    
+    else if(a>0){
+    printf("The number is a positive value.\n");
+    }
+
+    else{
+    printf("The number is 0 value.\n");
+    }
+
+    return 0;
+}
     
