3101076fb38dc36005c448bf5c6676bab9099323
Lusifer2602 <2602pranav@gmail.com>
2025-05-06 14:35:44 +0530

    bete factorial bhi banana aa gaya

--- Learning-C/learningboosted/factorial.c
+++ Learning-C/learningboosted/factorial.c
@@ -4,7 +4,7 @@ int main() {
     printf("Enter the number you want to find factorial of : ");
     scanf("%d", &num);//take input from user aboout what number to find fact. of
 
-    for(i=num; i>=1; i--){
+    for(i=num; i>=1; i--){ //we can also do this by incrementing i from 1 to the given input, abhi karte hain rukk
        factorial=factorial*i;
     if (num<0){printf("negative integers are not defined for factorial");}
     }
