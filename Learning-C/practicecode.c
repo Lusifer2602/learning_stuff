d72d0239739765b91396de4e11b7fb9d3abf7950
Lusifer2602 <2602pranav@gmail.com>
2025-05-11 00:46:27 +0530

    to see how many and which numbers are divisible by 2 but not by 4

--- Learning-C/practicecode.c
+++ Learning-C/practicecode.c
@@ -5,14 +5,18 @@ int main(){
 //problem 3 :  print numbers 1 to 20 that are not divisible by 2 (use ! operator)
 //problem 4 : Count how many numbers between 1 to n are divisible by 2 but not by 4
 
-int i, j, k; //one extra var for safety lol
-//Prolem 2
-printf("The following numbers from 1-50 are divisible by 3 or 7 : ");
-for(i=1;i<=50;i++){
-
-if(i%3==0 || i%7==0){printf("%d, ", i);}
+int i, j, k=0; //one extra var for safety lol
+//Prolem 4 (3 na kari ghani easy hai)
+printf("Enter the limit till where you want to see how many numbers are divisible by 2 but not by 4 : ");
+scanf("%d", &j);
+for(i=1;i<=j;i++){
+    if(i%2==0&&i%4!=0){
+        printf("%d ", i);
+        k++;
+    }
 }
-printf("\n");
+    printf("are the values divisible by 2 but not by 4."
+            "\nAnd there's total %d of them.\n", k++);
  
 return 0;
 }
