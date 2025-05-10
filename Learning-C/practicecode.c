5559783a27832d304677b1b9520bff3f0897f095
Lusifer2602 <2602pranav@gmail.com>
2025-05-10 23:42:58 +0530

    to print squares till the given number

--- Learning-C/practicecode.c
+++ Learning-C/practicecode.c
@@ -1,21 +1,12 @@
 #include<stdio.h>
 int main(){
 //lets find sum of n even natural numbers
-int i, newnum, sum=0, num;
-printf("Enter the number till where you want to find some of even numbers :  ");
+int i, square, num, rows;
+printf("Enter the number till where you want to find squares of : ");
 scanf("%d", &num);
-
-if(num%2!=0){
-    newnum=num-1;
+for(i=1; i<=num; i++){
+    printf("%d x %d = %d\n", i, i, i*i);
 }
-
-else{printf("Invalid Input! Please add a valid natural number");}
-
-    for(i=newnum;i>=2;i-=2){
-        sum=sum+i;
-    }
-    printf("The sum of all natural even number from 2 to %d is : %d\n", num, sum);
-
 
     return 0;
 }
