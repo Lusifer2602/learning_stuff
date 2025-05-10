68ef90f087a2a776d1b659a406ed364662ec62c7
Lusifer2602 <2602pranav@gmail.com>
2025-05-11 00:23:25 +0530

    Values 1-50 divisible by 3&5

--- Learning-C/practicecode.c
+++ Learning-C/practicecode.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 int main(){
-//lets find sum of n even natural numbers
-int i, sq, num;
-printf("Enter the number till where you want to find squares of : ");
-scanf("%d", &num);
-for(i=1; i<=num; i++){
-    sq=i*i;
-    printf("%d x %d = %d\n", i, i, sq);
-}
+//ok we have a problem where we want to print numbers 1 to 50 that are divisible by both 3 and 5
+//then problem 2 : print numbers 1 to 30 that are divisible by 3 or 7
+//problem 3 :  print numbers 1 to 20 that are not divisible by 2 (use ! operator)
+//problem 4 : Count how many numbers between 1 to n are divisible by 2 but not by 4
 
-    return 0;
+int i, j, k; //one extra var for safety lol
+//Prolem 1
+printf("The following numbers from 1-50 are divisible by 3 & 5 : ");
+for(i=1;i<=50;i++){
+
+if(i%3==0 && i%5==0){printf("%d, ", i);}
+}
+printf("\n");
+ 
+return 0;
 }
