f1d8671b0879e08c61c2153a75fdf4ca5f007cd0
Lusifer2602 <2602pranav@gmail.com>
2025-05-10 23:46:44 +0530

    another way to print squares of numbers

--- Learning-C/practicecode.c
+++ Learning-C/practicecode.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
 //lets find sum of n even natural numbers
-int i, square, num, rows;
+int i, sq, num;
 printf("Enter the number till where you want to find squares of : ");
 scanf("%d", &num);
 for(i=1; i<=num; i++){
-    printf("%d x %d = %d\n", i, i, i*i);
+    sq=i*i;
+    printf("%d x %d = %d\n", i, i, sq);
 }
 
     return 0;
