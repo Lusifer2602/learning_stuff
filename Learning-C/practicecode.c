ef6eb68351d3a9a3125f5eba3ec992f915b4ec7a
Lusifer2602 <2602pranav@gmail.com>
2025-05-10 21:27:03 +0530

    natural numbers ka sum kardia

--- Learning-C/practicecode.c
+++ Learning-C/practicecode.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 int main(){
-    //factorial bana ke dekhte hain
-    int i, num, factorial=1;
-    printf("Enter the number to find factorial of : ");
-    scanf("%d", &num);
-    
-    for(i=num;i>=1;i--){
-        factorial=factorial*i;
-    }
-    printf("Factorial of the following number is : %d\n", factorial);
+//lets find sum of n natural numbers
+int i, sum=0, num;
+printf("enter the number to find sum of : ");
+scanf("%d", &num);
+
+for(i=num;i>=1;i--){
+sum+=i;
+}
+printf("The sum of the following natural number is : %d\n", sum);
+
+
     return 0;
 }
