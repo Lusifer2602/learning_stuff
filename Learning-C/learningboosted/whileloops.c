0f53d7e1c2c0c44c89b5007a1f7fda93a2cfc5e9
Lusifer2602 <2602pranav@gmail.com>
2025-05-14 21:58:10 +0530

    managed to reverse a number and to print sum of values within an integer

--- Learning-C/learningboosted/whileloops.c
+++ Learning-C/learningboosted/whileloops.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 int main(){
-    int num=123, sum=0, rem;
+    int num=123, sum, rem; // this is a program to find sum of all the integers in a given natural number
     while(num>0){
         rem=num%10;
         num=num/10;
         sum+=rem;}
-    printf("%d \n", sum);
+    printf("%d ", sum);
+    
+    //next we will write a program  to reverse the numbers of an integer
+   int remainder, number=321;
+    while(number>0){
+        remainder=number%10;
+        number=number/10;
+        printf("%d", remainder);}
+    printf("\n");
 
-return 0;
+    return 0;
 }
