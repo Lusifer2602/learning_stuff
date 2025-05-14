f364cdb8a87dc5ff4bf864ec1cabbf4844fdfd6a
bosswhopper <94573164+Lusifer2602@users.noreply.github.com>
2025-05-14 11:54:14 +0000

    starting while loops

--- Learning-C/learningboosted/for1.c
+++ Learning-C/learningboosted/for1.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main(){
-    int i; //declare the loop variable
-    //now lets create a for loop in this
-    for (i=1; i<=5; i++){
-        printf("The number now is : %d\n", i);
-    }
-
-    return 0;
+    int i, j, k; 
+for(i=1;i<=5;i++){
+	for(j=i;j>=1;j--){
+	printf("* ");
+}
+printf("\n");
+}
+return 0;
 }
