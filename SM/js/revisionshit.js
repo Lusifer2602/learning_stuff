4e60258aa1e8f98e0ca49a5748f1b3ee8eb00a07
Lusifer2602 <2602pranav@gmail.com>
2025-05-16 02:28:34 +0530

    js mein comebackgit add .!

---
+++ SM/js/revisionshit.js
@@ -0,0 +1,16 @@
+// 1. Variables & Types
+let name="DL91 FM", year=2025, count=30, op=true;
+const by="TBSM";
+
+console.log(`type of ${name} is `, typeof(name));
+console.log(`type of ${year} is `, typeof(year));
+console.log(`type of ${by} is `, typeof(by));
+console.log(`type of ${op} is `, typeof(op));
+
+console.log(`${name} ne bhundeinfaad di afterall its ${year} \n and ${by} has released their new album with ${count} gems in it!!!! Fookin Overpowered shit, ${op} that`);
+//Arithmetic & Assignment operations
+let saal=2025;
+saal++;
+console.log("Next year will be : ", saal); 
+console.log("Year *  count = ", count*year);
+
