db5f58dc8b18559f5b2741ab7cccf1b8399a8b6b
Lusifer2602 <2602pranav@gmail.com>
2025-05-16 02:36:15 +0530

    logical ops tak krlia baabe

--- SM/js/revisionshit.js
+++ SM/js/revisionshit.js
@@ -8,9 +8,17 @@ console.log(`type of ${by} is `, typeof(by));
 console.log(`type of ${op} is `, typeof(op));
 
 console.log(`${name} ne bhundeinfaad di afterall its ${year} \n and ${by} has released their new album with ${count} gems in it!!!! Fookin Overpowered shit, ${op} that`);
+
+
 //Arithmetic & Assignment operations
 let saal=2025;
 saal++;
 console.log("Next year will be : ", saal); 
 console.log("Year *  count = ", count*year);
+
+
+//Boolean Logic
+console.log("" || "fallback");
+console.log(0&&"yes");
+console.log(null ?? "default");
 
