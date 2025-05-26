a146e672050af055cd94aebc52df3a27c23099a3
Lusifer2602 <2602pranav@gmail.com>
2025-05-26 23:23:56 +0530

    completed armstrong problem started knowing math.h deeper added file that lists important header files used in c(will explore them all as per the usecase overtime

--- Learning-C/learningboosted/armstrongnumber.c
+++ Learning-C/learningboosted/armstrongnumber.c
@@ -1,3 +1,21 @@
 #include<stdio.h>
-int main()
+int main(){
+    int sum=0, a, i, pow;
+    printf("Enter the number to check Armstrong number on : ");
+    scanf("%d", &a);
+    int original=a;
 
+    while(a>0){
+        i=a%10;
+        pow=i*i*i;
+        sum+=pow;
+        a/=10; //we need to remove the last digit of the number to go back to second last and continue to loop or it'll run forever
+    }
+
+if(sum==original){
+    printf("Yes %d is an Armstrong Number\n", sum);}
+else{
+    printf("No %d is not an Armstrong numer\n", original);}
+
+return 0;
+}
