c827198ffe79fe8812b1a95e5ad602d7fbdb4361
bosswhopper <94573164+Lusifer2602@users.noreply.github.com>
2025-05-27 20:37:00 +0000

    created a hypoteneus calculator(for int only abhi k liye)

--- Learning-C/learningboosted/iterate.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    int i, rem, quot, original, extra;
-    i=325434;
-    while(i>0){
-        rem=i%10;
-
--- Learning-C/mathheader/mathsfirst.c
+++ Learning-C/mathheader/mathsfirst.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    double a=9;
-    printf("We want to find square of 9\n"
-            "so it will be : %lf\n", pow(a, 3));
+    /* double a=9; */
+    /* int b = (int)pow(a, 2); */
+    /* printf("We want to find square of 9\n" */
+    /*         "so it will be : %d\n", b); */
 
+    /* int batman=9; */
+    /* printf("Square root of 9 will be : %d\n", (int)sqrt(batman)); */
+
+    /* double new=32.2; */
+    /* printf("Square of %.1lf will be %.3lf\n", new, pow(new, 2)); */
+
+//lets create a program that calculates the length of hypotenuse of a triangle
+int height, length, hypotenuse;
+printf("Enter the height and perpendicular \nlength should separated with a space : ");
+scanf("%d %d", &height, &length);
+
+hypotenuse=(int)(sqrt(pow(height, 2)+pow(length, 2)));
+
+printf("Alright that means the hypotenuse should be %d\n", hypotenuse);
+
+ return 0;
+}
