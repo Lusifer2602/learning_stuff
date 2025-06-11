016d30f344c053a631139b4d32faeda0475d1320
Lusifer2602 <2602pranav@gmail.com>
2025-06-11 13:55:56 +0530

    working on contact manager

--- pythonstuff/contacmanager.py
+++ pythonstuff/contacmanager.py
@@ -1,5 +1,21 @@
-import json
 #lets save some phone numbers with their respective names
 #and end the data entry with q as input
 #and enter delete to navigate to a place where person can delete the following username
 #the user will be asked if they want to remove the contact using their phone number or username
+
+import json
+print("This is a program that we use to manage different contacts")
+print("Enter q to quit the program otherwise keep adding your contacts :) ")
+
+while True:
+    
+    name = input("Enter name with surname here : ")
+    if name=='q':
+        break
+    number=int(input("Phone number : "))
+    contacts={'name':name,'number':number}   
+    if type(number)!=int:
+        print("Phone number has to be an integer")
+    with open('contact.json', 'a') as file:
+        json.dump(contacts, file)
+
--- pythonstuff/sample.py
+++
@@ -1,8 +0,0 @@
-import os
-a=input("Enter name of the file you want to create : ")
-
-with open(a, 'a') as file:
-    file.write('This is just a random text to test something')
-
-print("Now we will erase the same file")
-os.remove(a)
