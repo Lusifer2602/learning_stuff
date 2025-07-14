ca170acd4e3210df2c7ef6f4d66e2a1d05a614d4
Lusifer2602 <2602pranav@gmail.com>
2025-07-15 01:20:37 +0530

    practicing loops in comeback.c

--- Learning-C/learningboosted/comeback.c
+++ Learning-C/learningboosted/comeback.c
@@ -1,8 +1,16 @@
+//for loops
+//tables completed
+//patterns completed, 1-10 * and 10-1
+//print even numbers from 1 to 10
+//
 #include<stdio.h>
 int main(){
-    int rem, a, add;
-    rem=0;
-    printf("Enter the number you want to reverse : ");
+ int a,num;
+ for(a=1;a<=10;a++){
+     if(a%2==0){
+         printf ("%d ", a);
+     }
+ }printf("\n");
 
-        return 0;
+   return 0;
 }
