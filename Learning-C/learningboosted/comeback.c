9403de0bbd9dd2d105b635201c40383dab75ff16
Lusifer2602 <2602pranav@gmail.com>
2025-07-15 01:36:48 +0530

    started revision on while loops

--- Learning-C/learningboosted/comeback.c
+++ Learning-C/learningboosted/comeback.c
@@ -1,16 +1,13 @@
-//for loops
-//tables completed
-//patterns completed, 1-10 * and 10-1
-//print even numbers from 1 to 10
-// print numbers 1 to 50 divisible both by 3 and 5
+//while loops
+//print even numbers till 10
+// print numbers in reverse
 #include<stdio.h>
+
 int main(){
- int a,num;
- for(a=1;a<=50;a++){
-     if(a%3==0 && a%5==0){
-         printf("%d ", a);
-     }
-     else{continue;}
- }printf("\n ");
-   return 0;
+    int a=10;
+    while(a>=1){
+        printf("%d ", a);
+        a--;
+   } printf("\n");
+    return 0;
 }
