ea9df02e1514ed552a71445d462e29da67ed0ff0
Lusifer2602 <2602pranav@gmail.com>
2025-07-15 01:28:56 +0530

    for loops revised moving forward to while

--- Learning-C/learningboosted/comeback.c
+++ Learning-C/learningboosted/comeback.c
@@ -2,15 +2,15 @@
 //tables completed
 //patterns completed, 1-10 * and 10-1
 //print even numbers from 1 to 10
-//
+// print numbers 1 to 50 divisible both by 3 and 5
 #include<stdio.h>
 int main(){
  int a,num;
- for(a=1;a<=10;a++){
-     if(a%2==0){
-         printf ("%d ", a);
+ for(a=1;a<=50;a++){
+     if(a%3==0 && a%5==0){
+         printf("%d ", a);
      }
- }printf("\n");
-
+     else{continue;}
+ }printf("\n ");
    return 0;
 }
