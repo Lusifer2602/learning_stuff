4efc869a2f46a28b5ab42862ad2ab47813e76c33
Lusifer2602 <2602pranav@gmail.com>
2025-07-15 23:14:05 +0530

    revised palindrome sexier than ever infact with all checks for negatives that aren't palindrome ever

--- Learning-C/learningboosted/comeback.c
+++ Learning-C/learningboosted/comeback.c
@@ -3,24 +3,26 @@
 // print nums in reverse
 // count num of digits in an intger
 // print a num in reverse order
+// check palindrome
 //
 #include<stdio.h>
 
 int main(){
     int rem, num, a=0;
-    printf("Enter the num you want to reverse : ");
+    printf("Enter the num you want to see if is a palindrome : ");
     scanf("%d", &num);
-    if(num==0){
-        printf("You stupid fuck thats 0, reverse it and it is still the same\n");
-    }
-    else{
-        while(num!=0){
+
+    int save=num;
+    if(num<0){printf("This number is negative hence can't be a palindrome sir\n");}
+
+        else{while(num!=0){
             rem=num%10;
             a=(a*10)+rem;
             num/=10;
         }
-        printf("%d\n", a);
-    }
+        if(save==a){
+            printf("Yes the number is a palindrome\n");}
+        else {printf("No you stupid fuck it isn't no palindrome\n");}}
             
        
        return 0;
