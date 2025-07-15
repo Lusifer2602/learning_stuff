0e58bdeea6707a690b2dd22e4d2d47a6549d65fc
Lusifer2602 <2602pranav@gmail.com>
2025-07-15 18:36:55 +0530

    created a digit counter for integers added checks for positive negative and 0 to ensure it works for all integers (not the decimals ofcourse (for now)

--- Learning-C/learningboosted/comeback.c
+++ Learning-C/learningboosted/comeback.c
@@ -1,13 +1,27 @@
 //while loops
 //print even numbers till 10
 // print numbers in reverse
+// count number of digits in an intger
+//
 #include<stdio.h>
 
 int main(){
-    int a=10;
-    while(a>=1){
-        printf("%d ", a);
-        a--;
-   } printf("\n");
-    return 0;
+    int count=0, n;
+    printf("Enter the number : ");
+    scanf("%d", &n);
+    
+    if(n==0){
+        count=1;}
+    else if(n<0){
+        n=-n;
+        while(n>0){
+        count++;
+        n/=10;}
+    }
+    else {while(n>0){
+        count++;
+        n/=10;}}
+    printf("%d\n", count);
+    
+       return 0;
 }
