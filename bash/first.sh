7aae27b14f728d7e52ee30aad4e578f2e605665f
Lusifer2602 <2602pranav@gmail.com>
2025-07-26 18:25:16 +0530

    started learning bash again

---
+++ bash/first.sh
@@ -0,0 +1,7 @@
+ls
+pwd
+# bash runs as an intepreter so the commands are run one by one till it runs into an error
+errortext
+#and here we will see that after running first two files the interpreter crashes and throws error
+
+
