80384237f7a06ce3dcd8621368f5dd5fdd4f4b30
Lusifer2602 <2602pranav@gmail.com>
2025-07-26 18:31:27 +0530

    just hello world of bash done

--- BASH/himom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Hi mom"
-sleep 3
-echo "uhh huh"
-sleep 2
-echo "abbe jaa na bakwas kar rha!!!!"
--- bash/first.sh
+++ bash/first.sh
@@ -1,7 +1,5 @@
-ls
+#!/bin/bash
+#First line of bash always tells the system which interpreter to start runnin
 pwd
-# bash runs as an intepreter so the commands are run one by one till it runs into an error
-errortext
-#and here we will see that after running first two files the interpreter crashes and throws error
-
-
+echo "Hello, I am Batman"
+echo "this is the first script"
