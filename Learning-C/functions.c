2b0f6e27fd944090bd123eb73c210a00e29f4ca6
bosswhopper <94573164+Lusifer2602@users.noreply.github.com>
2025-07-28 04:59:49 +0000

    "committed to resolve merge conflicts"

--- Learning-C/functions.c
+++ Learning-C/functions.c
@@ -20,17 +20,15 @@ functionName(arguments);    */
 //function with parameters, no retrun
 //
 //
-void birthdate(){
-    printf("Please enter your age to know what year you were born in : ");
-    int age;
-    scanf("%d", &age);
-    printf("So you were born in the year : %d\n", 2025-age);
+
+int add (int, int); //optional step
+
+int add(int a, int b){
+    return a+b;
 }
 
-int main() {
-    birthdate();
-
+int main(){
+    int result=add(5, 7);
+    printf("Output is : %d\n", result);
     return 0;
 }
-
-
