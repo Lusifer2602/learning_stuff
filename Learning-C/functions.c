7a3a35cee73fe882633cd6fc46442d79151aa61f
Lusifer2602 <2602pranav@gmail.com>
2025-07-30 21:07:02 +0530

    MADE THE RIGHT ANGLED TRIANGLE PROGRAM

--- Learning-C/functions.c
+++ Learning-C/functions.c
@@ -2,29 +2,6 @@
 //functions calling other functions
 //even odd checking using functino in function method
 
-/* Triangle Utility Design (Modular & Functional)
-Main Menu
-
-Ask the user:
-
-1 to find hypotenuse
-
-2 to find perpendicular side
-
-Functions you'll need:
-
-float findHypotenuse(float base, float height);
-
-float findPerpendicular(float hypotenuse, float base);
-
-Inside the main()
-
-Take user input for choice
-
-Then prompt for required values based on that
-
-Call appropriate function and print result with 2 decimal precision*/
-
 int isEven(int a){
     return a%2;
 }
