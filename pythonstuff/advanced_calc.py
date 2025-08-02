d60d154bc4f3898dc9ff1de2415b9b0a8623e2c5
Lusifer2602 <2602pranav@gmail.com>
2025-08-02 23:58:35 +0530

    Working on an advanced calc for the fun of it

---
+++ pythonstuff/advanced_calc.py
@@ -0,0 +1,38 @@
+import math
+choice=int(input('''
+1. Basic Arithmetic & Algebraic Computations:
+    a. absolute value
+    b. addition (+)
+    c. ceiling (round up)
+    d. division (/, ÷)
+    e. exponential growth
+    f. factorial
+    g. floor (round down)
+    h. gcd (greatest common divisor)
+    i. lcm (least common multiple)
+    j. logarithmic calculations
+    k. multiplication (x, ×)
+    l. power calculation
+    m. square root
+    n. subtraction (-)
+    o. truncate decimal 
+2. Trigonometric Calculations:
+
+    a. angle conversion (radians <-> degrees)
+    b. cos (cosine)
+    c. inverse trigonometric functions
+    d. sin (sine)
+    e. tan (tangent)
+3. Geometric Calculations:
+    a. area of a circle
+    b. area of a square
+    c. area of a triangle
+    d. perimeter and volume formulas
+    e. surface area of a cylinder
+    f. surface area of a sphere : ''')) #user decides here what to do
+
+def two_nums():
+    a=int(input('Enter the first num : '))
+    b=int(input('Enter the second num : '))
+def basic_arith:
+
