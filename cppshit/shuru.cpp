725ebd2694e1546d7f4a7e6c506737b8dc8d4486
Lusifer2602 <2602pranav@gmail.com>
2025-08-14 21:21:09 +0530

    started learning Cpp as well (this is going to be such a drag

---
+++ cppshit/shuru.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std; //helps compiler not confuse use of same keywords at different parts of the program
+
+int main() {
+    cout << "Here's the first output" //printf statement but in C++
+            "\nNow is when we start to learn and build shit in C++\n";
+return 0; //same as cpp
+}
