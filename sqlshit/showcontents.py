6f5337c9b0040cc3e0ff5c2f3e9c65a229c623c4
bosswhopper <94573164+Lusifer2602@users.noreply.github.com>
2025-08-27 07:58:56 +0000

    can add and access the entire list at once

--- sqlshit/showcontents.py
+++ sqlshit/showcontents.py
@@ -9,27 +9,41 @@ c.execute("""
     kaksha int,
     grade TEXT
 )""") #created a table under the database here
+#   INTEGER PRIMARY KEY WILL INDEX USERS IF WE PRINT ALL THE CONTENTS OF THE DATABASE
 
-print("We are going to store name , class and grades of students"
-      "you can quit the program anytime by just pressing q into any of the inputs you are being asked"
-      "\n NOTE : if you terminate a program before the 3 values are entered then that one column just won't be saved")
+print("We are going to store name , class and grades of students\n"
+      "you can quit the program anytime by just pressing q into any of the inputs you are being asked\n"
+      "\n NOTE : if you terminate a program before the 3 values are entered then that user will not be saved\n")
 
 while True:
-    naam=input("Enter your name : ")
+    name=input("Enter your name : ")
     if name=='q':
         break
 
     while True: #for taking input for kaksha and redirecting the user back to entering input again if they get it wrong
-        try:
-            kaksha=int(input("Enter your age : "))
+        kaksha=input("Enter the class you read in : ")
+        if kaksha=='q':
             break
-        
+        try:
+            kaksha=int(kaksha)
+            break 
         except:
             print("Wrong type input try again")
+    if kaksha=='q':
+        break
 
+    grade=input("Enter what grade they received : ","\n")
+    if grade=='q':
+        break
 
+    c.execute("INSERT INTO scores (name, kaksha, grade) VALUES (?,?,?)", (name, kaksha, grade))
+    conn.commit()
 
-c.execute("SELECT * FROM TABLE scores")
+print("\nHere's the list of all the entries you have made so far \n")
+
+c.execute("SELECT * FROM scores")
 rows=c.fetchall()
 for row in rows:
     print(row)
+
+conn.close()
