b2bec6d1ac1c3af26ddfa3fdc9fbba2ef8f6701d
bosswhopper <94573164+Lusifer2602@users.noreply.github.com>
2025-08-28 13:31:48 +0000

    learning conditionals for sqlite not added to the code; codespace crashed didn't save in time

--- sqlshit/sqlshit.py
+++ sqlshit/sqlshit.py
@@ -1,35 +1,30 @@
-import sqlite3 as sq
+jmport sqlite3 as sq
 
 #now let us connect with the sqlite server
 conn=sq.connect("details.db")
 
 # create a cursor (like a pen) to write sql commands
 c=conn.cursor()
-
+print("Alright lets save some data")
 #create a table
-c.execute("""
-    CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        mobile,
-        mail TEXT
-)
-""")
+c.execute("SELECT * FROM marks");
 #CREATE TABLE IF NOT EXISTS will create a table within the database if it doesn't exist already
 #c.execute will let me execute commands through the cursor into the database
-naam=input("Please enter your name : ")
+while True:
+    name=input(" Name : ")
 
-while True: #error check for saving mobile number to the database
-    try:
-        number=int(input("Enter your mobile number : "))
-        break
-    except:
-        print("Invalid Type of Input try again")
+    while True: #error check for saving mobile number to the database
+        score=input("Marks : ")
+        if score=='q':
+            break
+        try:
+            score=int(score)
+            break
+        except:
+            print("Invalid type of input try again\n")
 
-em=input("Enter your email id : ")
-
-#now we will add data to the database ; here goes one
-c.execute("INSERT INTO students (name, mobile, mail) VALUES (?,?,?)", (naam, number, em)) #this will add the following details to the database
+    # here we will add data to the database
+    c.execute("INSERT INTO students (name, score) VALUES (?,?)", (name, score))
 
 conn.commit() #save changes to the database
 conn.close() #close connection with database
