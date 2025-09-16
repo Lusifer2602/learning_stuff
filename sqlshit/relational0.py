86c8802088d0246e3a64bbe063a79317bab0b30a
bosswhopper <94573164+Lusifer2602@users.noreply.github.com>
2025-09-16 07:16:35 +0000

    learnt linking and printing data after joining 2 DBs

--- sqlshit/relational0.py
+++ sqlshit/relational0.py
@@ -2,11 +2,28 @@ import sqlite3 as sq
 con=sq.connect('sample.db')
 c=con.cursor()
 
-row=c.execute("""SELECT students.name, students.age, grades.subject, grades.grade
-          FROM students
-          JOIN grades on students.student_id=grades.student_id
-          """)
+row=c.execute("""
+SELECT students.name, students.age, grades.subject, grades.grade
 
-for rows in row:
-    print(rows)
+FROM students JOIN grades on students.student_id=grades.student_id
 
+WHERE name='Kamal Kumar'
+""")
+# students.name, students.age, grades.subject are the different columns we are picking in the format of <tablename.columnname>.
+
+#FROM students JOIN grades --> this line joins the two tables together 
+
+# after JOIN is the condition < on students.student_id=grades.student_id > that states that join the tables from the point where in students(table) the student_id(column name) is equal to student_id(column name) in grades(table)
+
+for i in row:
+    print(i)
+
+print("This is students table data")
+rows=c.execute("SELECT * FROM students")
+for i in rows:
+    print(i)
+
+rows=c.execute("SELECT * FROM GRADES")
+print("This is grades table data")
+for i in rows:
+    print(i)
