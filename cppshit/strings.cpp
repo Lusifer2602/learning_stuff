893fdd8f4e55600b5101c44e506873cfac9ab566
Lusifer2602 <2602pranav@gmail.com>
2025-12-03 05:09:55 +0530

    length of strings using size method

--- cppshit/strings.cpp
+++ cppshit/strings.cpp
@@ -11,10 +11,10 @@ int main(){ //exploring what and how we can work with strings cuz apparently CPP
             string combined = batman+superman+deadpool+spiderman; //this is called concatination of strings
             cout << combined << "\n";
             //append - alright there is somethign called appent that is actually a function in cpp to be used on strings if needed
-            string name=batman.append(superman),  
-                   name0=spiderman.append(deadpool).append(batman); //so here we see you can also concatinate at the end of a string more than once
-            cout << name << ' ' << name0 << "\n";
-
+                   string name0=spiderman.append(deadpool).append(batman); //so here we see you can also concatinate at the end of a string more than once
+            
+            cout << "Length of the value of variable superman is : " << superman.size() << "\n" <<
+            "\nLength of the var at Batman will be : " << batman.size() << "\n";
 
     return 0;
 }
